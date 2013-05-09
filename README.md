b5c114ad6f80e538d587a632f39a2dd64ca07e25
Nishibayashi Takuji <takuji31@gmail.com>
2013-05-10 03:09:53 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Task::Takuji - It's new $module
+
+# SYNOPSIS
+
+    use Task::Takuji;
+
+# DESCRIPTION
+
+Task::Takuji is ...
+
+# LICENSE
+
+Copyright (C) Nishibayashi Takuji.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Nishibayashi Takuji <takuji31@gmail.com>
---
+++ lib/Task/Takuji.pm
@@ -0,0 +1,39 @@
+package Task::Takuji;
+use 5.008005;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Task::Takuji - It's new $module
+
+=head1 SYNOPSIS
+
+    use Task::Takuji;
+
+=head1 DESCRIPTION
+
+Task::Takuji is ...
+
+=head1 LICENSE
+
+Copyright (C) Nishibayashi Takuji.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Nishibayashi Takuji E<lt>takuji31@gmail.comE<gt>
+
+=cut
+
